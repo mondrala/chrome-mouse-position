4d7db4921d1f3076447edb06e61eac586bd7aa2a
Robola stanislaw <testalofreee@hotmail.com>
2018-07-30 00:05:31 +0200

    Avoiding Chrome internal pages for allowing the injection of content scripts

--- chrome-mouse-position/js/bg.js
+++ chrome-mouse-position/js/bg.js
@@ -1,43 +1,16 @@
 
 
-
-// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tabs) {
-// code in here will run every time a user goes onto a new tab, so you can insert your scripts into every new tab
-
-
- // for (var i = 0; i < tabs.length; i++) {
-	 
-// var tabUrl = tabs[i].url;
-        // if (tabUrl == 'chrome://extensions/') {
-		// console.log('nothing');
-	
-		// }
-		
-		
-		// else{
-			
-			
-			// chrome.tabs.executeScript(tabId, {file: "js/position.js"});
-
-			
-			// }
-
- // }
-
-// });
-
-
  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-      //... whatever other stuff you were doing anyway
+
 chrome.tabs.getSelected(null, function(tab) {
 	
 	
 	
-        // tab = tab.id;
+ 
         var tabUrl = tab.url;
 
 		        if (tabUrl == 'chrome://extensions/') {
-					console.log('nothing');
+					console.log('nothing to do at this page');
 	
 		}
 		
@@ -47,13 +20,11 @@ chrome.tabs.getSelected(null, function(tab) {
 			
 			chrome.tabs.executeScript(null, {file: "js/position.js"});
 			
-						// chrome.tabs.executeScript(tabId, {file: "js/position.js"});
-
-
+				
 			
 			}
 		
-        // alert(tab.url);
+   
     });
 	  });
 
@@ -61,11 +32,3 @@ chrome.tabs.getSelected(null, function(tab) {
 
 
 
-
-// document.addEventListener("DOMContentLoaded", function () {
-    // document.getElementById('tmp-clipboard').onclick = function () {
-        // chrome.tabs.executeScript(null, {
-            // file: "js/position.js"
-        // });
-//}
-// });
